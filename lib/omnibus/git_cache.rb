7ba64a4aaa486d4db08e5f9909f056afad77521e
Isa <ifarnik@llnw.com>
2017-03-24 14:56:20 -0700

    git_cache: join git command lines to avoid mangle
    obvious fix?
    
    Signed-off-by: Isa <ifarnik@llnw.com>

--- lib/omnibus/git_cache.rb
+++ lib/omnibus/git_cache.rb
@@ -191,11 +191,14 @@ module Omnibus
     # @return [Mixlib::Shellout] the underlying command object.
     #
     def git_cmd(command)
-      shellout!("git -c core.autocrlf=false \
-                     -c core.ignorecase=false \
-                     --git-dir=#{cache_path} \
-                     --work-tree=#{install_dir} \
-                     #{command}".gsub(/\s+/, " "))
+      shellout!([
+        "git",
+        "-c core.autocrlf=false",
+        "-c core.ignorecase=false",
+        "--git-dir=\"#{cache_path}\"",
+        "--work-tree=\"#{install_dir}\"",
+        command,
+      ].join(" "))
     end
 
     #
